218a650f83409c7521ce405bbfe5756c06661774
liudong <liudong.edward@gmail.com>
2019-05-09 09:34:13 +0800

    整理代码

---
+++ Sources/Codable/BaseBinaryEncodingContainer.swift
@@ -0,0 +1,88 @@
+//
+//  BaseBinaryEncodingContainer.swift
+//  ANBinaryCodable
+//
+//  Created by 刘栋 on 2019/5/9.
+//  Copyright © 2019 anotheren.com. All rights reserved.
+//
+
+import Foundation
+
+public protocol BaseBinaryEncodingContainer {
+    
+    mutating func write(data: Data)
+    mutating func encode<S>(_ sequence: S) where S: Sequence, S.Element == UInt8
+    mutating func encode(_ value: Int8, using endian: Endian)
+    mutating func encode(_ value: Int16, using endian: Endian)
+    mutating func encode(_ value: Int32, using endian: Endian)
+    mutating func encode(_ value: Int64, using endian: Endian)
+    mutating func encode(_ value: UInt8, using endian: Endian)
+    mutating func encode(_ value: UInt16, using endian: Endian)
+    mutating func encode(_ value: UInt24, using endian: Endian)
+    mutating func encode(_ value: UInt32, using endian: Endian)
+    mutating func encode(_ value: UInt64, using endian: Endian)
+    mutating func encode(_ value: Float, using endian: Endian)
+    mutating func encode(_ value: Double, using endian: Endian)
+}
+
+extension BaseBinaryEncodingContainer {
+    
+    public mutating func encode<S>(_ sequence: S) where S : Sequence, S.Element == UInt8 {
+        write(data: Data(sequence))
+    }
+    
+    public mutating func encode(_ value: Int8, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: Int16, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: Int32, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: Int64, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: UInt8, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: UInt16, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: UInt24, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: UInt32, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: UInt64, using endian: Endian) {
+        let data = value.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: Float, using endian: Endian) {
+        let data = value.bitPattern.update(endian: endian).data
+        write(data: data)
+    }
+    
+    public mutating func encode(_ value: Double, using endian: Endian) {
+        let data = value.bitPattern.update(endian: endian).data
+        write(data: data)
+    }
+}
